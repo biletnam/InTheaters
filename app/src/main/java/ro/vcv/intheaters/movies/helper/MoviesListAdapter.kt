c937bddb7e9ce2ceb42225388a362c5b59e1576c
Vlad Voinescu <vladcvoinescu@gmail.com>
2018-10-17 17:05:21 +0300

    updated placeholder for movie image loading on the MovieList screen

--- app/src/main/java/ro/vcv/intheaters/movies/helper/MoviesListAdapter.kt
+++ app/src/main/java/ro/vcv/intheaters/movies/helper/MoviesListAdapter.kt
@@ -5,6 +5,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import ro.vcv.intheaters.movies.R
 import ro.vcv.intheaters.movies.models.Movie
 
@@ -27,6 +28,8 @@ class MoviesListAdapter(private val moviesList: List<Movie>) : RecyclerView.Adap
         if (position < moviesList.size) {
             Glide.with(context!!)
                     .load(moviesList[position].posterPath)
+                    .apply(RequestOptions()
+                            .placeholder(R.drawable.ic_movie_white_64dp))
                     .into(holder.movieImage)
         }
     }
